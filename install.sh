3aa40a04e8278fddc4ea38becc267e9b6772fadc
Guilherme Daher <65228950+daher13@users.noreply.github.com>
2025-04-15 21:34:02 -0300

    adjust path setting

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ mkdir -p $INSTALL_DIR/bin
 curl $GIT_URL > $INSTALL_DIR/bin/dotvm
 
 echo >> ~/.bashrc
-echo "DOTVM_PATH=\"$HOME/$INSTALL_DIR\"" >> ~/.bashrc
+echo "DOTVM_PATH=\"\$HOME/$INSTALL_DIR\"" >> ~/.bashrc
 echo 'if [ -d "$DOTVM_PATH" ]; then' >> ~/.bashrc
 echo '    export PATH="$DOTVM_PATH/dotnet-sdk:$DOTVM_PATH/bin:$PATH"' >> ~/.bashrc
 echo 'fi' >> ~/.bashrc
